4bda27381035a4188cc03843c036bc7d0360d420
asz33@cornell.edu <asz33@cornell.edu>
2021-01-14 10:54:24 -0800

    Remove video

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Video from "../../videos/video.mp4";
+// import Video from "../../videos/video.mp4";
 import { Button } from "../ButtonElements";
 import {
     HeroContainer,
@@ -23,7 +23,7 @@ const HeroSection = () => {
     return (
         <HeroContainer id="home">
             <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+                {/* <VideoBg autoPlay loop muted src={Video} type="video/mp4" /> */}
             </HeroBg>
             <HeroContent>
                 <HeroH1>Virtual Banking Made Easy</HeroH1>
