1646e177a9fbe1874b80d6ccd4974f4e5a681253
asz33@cornell.edu <asz33@cornell.edu>
2021-01-15 15:51:43 -0800

    Fix height

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -24,7 +24,7 @@ export const InfoWrapper = styled.div`
     display: grid;
     z-index: 1;
     width: 100%;
-    height: 90%;
+    height: 100%;
     max-width: 1100px;
     margin-right: auto;
     margin-left: auto;
